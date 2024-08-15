0eafc090745e76bcd13d83acb1cfe5bb85d8bf2c
githoboman <smithmar681@gmail.com>
2024-08-15 13:06:23 +0100

    khs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,8 +14,8 @@ export default function Home() {
   return (
    <>
    < div className ="position shadow-2xl bg-gray-900 bg-opacity-70  w-96 h-96 border-none rounded-2xl  " >
-    <h1  >
-      login here
+    <h1   >
+      admin panel
     </h1>
     <Card className="w-full max-w-sm">
       <CardHeader>
